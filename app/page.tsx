6907260ed298a58b02f2d2897424605d65b4efd3
Wellington Pinho <wellpinho@outlook.com>
2025-01-07 20:08:22 -0300

    structure

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>home page</h1>
+            <Button>Click</Button>
+        </div>
+    );
+}
